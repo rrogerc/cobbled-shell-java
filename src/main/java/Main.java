edcbf9c689d27869fcbd9b61b73105b098dedec6
Roger Chen <rogerchen803@gmail.com>
2025-02-09 18:28:48 -0700

    add echo builtin properly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,20 @@ public class Main {
             String input = scanner.nextLine();
             if (Objects.equals(input, "exit 0"))
                 break;
+            String[] command = input.split("\s+");
 
-            System.out.println(input + ": command not found");
+            if (command.length == 0)
+                continue;
+            if (Objects.equals(command[0], "echo")) {
+                for (int i = 1; i < command.length; i++) {
+                    System.out.print(command[i]);
+                    if (i != command.length - 1)
+                        System.out.print(" ");
+                }
+                System.out.println("");
+            }
+            else
+                System.out.println(input + ": command not found");
         }
     }
 }
