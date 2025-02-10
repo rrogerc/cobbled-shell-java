50479b22ab2125b809f4c619584d0ce6ecccc941
Roger Chen <rogerchen803@gmail.com>
2025-02-10 09:31:29 -0700

    handle PATH in type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,43 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    static void handle_type_command(String[] command) {
+        String[] path_directories = System.getenv("PATH").split(":");
+
+        String[] builtins = {"echo", "exit", "type"};
+        Arrays.sort(builtins);
+
+        if (command.length == 1)
+            return;
+        else if (Arrays.binarySearch(builtins, command[1]) >= 0) {
+            System.out.println(command[1] + " is a shell builtin");
+            return;
+        }
+
+        for (String dir : path_directories) {
+            Path path = Path.of(dir);
+            if (Files.isExecutable(path)){
+                System.out.println(command[1] + " is " + path.toString());
+                return;
+            }
+        }
+
+        System.out.println(command[1] + ": not found");
+    }
+
+    static void handle_echo_command(String[] command) {
+        for (int i = 1; i < command.length; i++) {
+            System.out.print(command[i]);
+            if (i != command.length - 1)
+                System.out.print(" ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
@@ -16,28 +51,13 @@ public class Main {
 
             if (command.length == 0)
                 continue;
-            else if (Objects.equals(command[0], "echo")) {
-                for (int i = 1; i < command.length; i++) {
-                    System.out.print(command[i]);
-                    if (i != command.length - 1)
-                        System.out.print(" ");
-                }
-                System.out.println();
-                continue;
-            }
-            else if (Objects.equals(command[0], "type")) {
-                String[] builtins = {"echo", "exit", "type"};
-                Arrays.sort(builtins);
-                if (command.length == 1)
-                    continue;
-                if (Arrays.binarySearch(builtins, command[1]) >= 0)
-                    System.out.println(command[1] + " is a shell builtin");
-                else
-                    System.out.println(command[1] + ": not found");
-                continue;
-            }
 
-            System.out.println(input + ": command not found");
+            if (Objects.equals(command[0], "echo"))
+                handle_echo_command(command);
+            else if (Objects.equals(command[0], "type"))
+                handle_type_command(command);
+            else
+                System.out.println(input + ": command not found");
         }
     }
 }
